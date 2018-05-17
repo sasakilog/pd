bd7023d5ead75f03ece23b4d28afd46cfbf7478f
sasakilog <tremoro@gmail.com>
2018-05-17 09:35:09 +0900

    Unity Project
    first commit

---
+++ TestPD/Assets/ItemList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemList : MonoBehaviour {
+
+    public GameObject canvas;
+    public GameObject ItemPanelPrefab;
+
+    GameObject ip;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("start");
+        ip = Instantiate(ItemPanelPrefab) as GameObject;
+        ip.transform.SetParent(canvas.transform, false);
+       // ip.transform.position = new Vector3(0,0,0);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        ip.transform.position.y--;
+     }
+}
